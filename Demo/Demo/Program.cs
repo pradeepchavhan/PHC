19c7929cfbcf40278200bf5a730566eaa5dd113a
pradeepchavhan <pradeepchavhan.engg@gmail.com>
2018-07-01 09:06:57 +0530

    'test'

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter First No: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Second No: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(string.Format("Additon of 2 no is:{0}",a+b));
+            //Console.ReadLine();
+        }
+    }
+}
